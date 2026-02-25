a46801e03abf7a7766e11992b493730c0171097d
arejAlmrai <areejalmree@gmail.com>
2026-02-25 15:34:21 +0300

    Creating application files and preparing the database

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('دليل سياحي')),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 7 / 8,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
