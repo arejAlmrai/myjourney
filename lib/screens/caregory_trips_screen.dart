fa2055825c0065d8c32128931118b3ad3425b6f9
arejAlmrai <areejalmree@gmail.com>
2026-02-27 18:19:59 +0300

    Navigator push

---
+++ lib/screens/caregory_trips_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CaregoryTripsScreen extends StatelessWidget {
+ 
+  final String? categoryId;
+  final String? categoryTitle;
+  const CaregoryTripsScreen({super.key, this.categoryId, this.categoryTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('صفحة الرحلات '),
+      ),
+      body: const Center(
+        child: Text('قائمة  برحلات هذا التصنيف'),
+      ),
+    );
+  }
+}
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,46 +1,65 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_app/screens/caregory_trips_screen.dart';
+import 'package:travel_app/screens/categories_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String? id;
   final String? title;
   final String? imageUrl;
-  CategoryItem({super.key, this.title, this.imageUrl});
+  CategoryItem({super.key, this.title, this.imageUrl, this.id});
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.push(ctx, MaterialPageRoute(builder: (ctx) => CaregoryTripsScreen(categoryId: id, categoryTitle: title)));
+    
+    @override
+    Widget build(BuildContext context) {
+      return Container();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-            
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15),
+    return InkWell
+    (
 
-          child: Image.network(
-            imageUrl!,
-            fit: BoxFit.cover,
-
-            width: double.infinity,
-            height: double.infinity,
-          ),
-        ),
-
-        Container(
-          padding: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-
-          child: Text(
-            title!,
-            style: const TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(15),
+      child: Stack(            
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+      
+            child: Image.network(
+              imageUrl!,
+              fit: BoxFit.cover,
+      
+              width: double.infinity,
+              height: double.infinity,
             ),
           ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-
-            color: Colors.black.withOpacity(0.4),
+      
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+      
+            child: Text(
+              title!,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+      
+              color: Colors.black.withOpacity(0.4),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
