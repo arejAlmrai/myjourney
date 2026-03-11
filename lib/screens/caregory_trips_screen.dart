3a396cbebeb8d495999902a3d2ec48d67ae4291c
arejAlmrai <areejalmree@gmail.com>
2026-03-11 19:00:36 +0300

    design Card Trip in Category Screen

--- lib/screens/caregory_trips_screen.dart
+++ lib/screens/caregory_trips_screen.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/app_data.dart';
+import 'package:travel_app/widgets/trip_item.dart';
 
 class CaregoryTripsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeArgument =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>?;
-    final categoryId = routeArgument?['id'] ;
-    final categoryTitle = routeArgument?['title'] ;
-    final filterTrips=Trips_data.where((trip){
+    final categoryId = routeArgument?['id'];
+    final categoryTitle = routeArgument?['title'];
+    final filteredTrips = Trips_data.where((trip) {
       return trip.categories.contains(categoryId);
     }).toList();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('صفحة الرحلات '),
+      appBar: AppBar(title: const Text('صفحة الرحلات ')),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return TripItem(
+            title: filteredTrips[index].title,
+            imageUrl: filteredTrips[index].imageUrl,
+            duration: filteredTrips[index].duration,
+            tripType: filteredTrips[index].tripType,
+            season: filteredTrips[index].season,
+          );
+        },
+
+        itemCount: filteredTrips.length,
       ),
-      body: ListView.builder(itemBuilder: (ctx,index){
-        return Text(filterTrips[index].title);
-      },
-       
-      itemCount:filterTrips.length ,
-      
-      ),
-    
     );
   }
 }
