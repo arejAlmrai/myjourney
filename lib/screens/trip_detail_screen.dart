3b86a991a6d9669e3a8c8d25ee7f8ff65e4d47fa
arejAlmrai <areejalmree@gmail.com>
2026-03-12 07:59:48 +0300

    desgin trip details screen

--- lib/screens/trip_detail_screen.dart
+++ lib/screens/trip_detail_screen.dart
@@ -1,16 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/app_data.dart';
 
 class TripDetailScreen extends StatelessWidget {
   const TripDetailScreen({super.key});
   static const screenRoute = '/trip_detail';
+  Widget buildSectionTitle(BuildContext context, String titleText) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      alignment: Alignment.topRight,
+
+      child: Text(
+        titleText,
+        style: TextStyle(color: Colors.blue, fontSize: 20),
+      ),
+    );
+  }
+
+  Widget buildListViewContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: BoxBorder.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 200,
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.symmetric(horizontal: 15),
+      child: child,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final tripId = ModalRoute.of(context)?.settings.arguments as String;
+    final selectedTrip = Trips_data.firstWhere((trip) => trip.id == trip.id);
 
     return Scaffold(
-      appBar: AppBar(title: Text('$tripId')),
-      body: Center(child: Text("هذه صفحة تقاصيل الرحلة$tripId ")),
+      appBar: AppBar(title: Text('${selectedTrip.title}')),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.network(selectedTrip.imageUrl, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 10),
+            buildSectionTitle(context, "الأنشطة"),
+            buildListViewContainer(
+              ListView.builder(
+                itemCount: selectedTrip.activities.length,
+                itemBuilder: (ctx, index) => Card(
+                  elevation: 0.3,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(selectedTrip.activities[index]),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            buildSectionTitle(context, "البرنامج اليومي"),
+            buildListViewContainer(
+              ListView.builder(
+                itemCount: selectedTrip.program.length,
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(child: Text("يوم ${index + 1}")),
+                      title: Text(selectedTrip.program[index]),
+                    ),
+                    Divider(),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
     );
   }
 }
