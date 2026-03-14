388b588ccb7460a24fd05093f80dd47315d023e9
arejAlmrai <areejalmree@gmail.com>
2026-03-14 21:19:25 +0300

    Programming and developing the filtering screen

--- lib/screens/filters_screens.dart
+++ lib/screens/filters_screens.dart
@@ -1,16 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/widgets/app_drawer.dart';
 
-class FiltersScreens extends StatelessWidget {
+class FiltersScreens extends StatefulWidget {
   const FiltersScreens({super.key});
 
   static const screenRoute = "/filters";
+
+  @override
+  State<FiltersScreens> createState() => _FiltersScreensState();
+}
+
+class _FiltersScreensState extends State<FiltersScreens> {
+  var _isInSummer = false;
+  var _isInWinter = false;
+  var _isForFamily = false;
+
+  Widget buildSwitchListTile(
+    String title,
+    String subtitle,
+    var currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: currentValue,
+      onChanged: updateValue(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: AppDrawer(),
       appBar: AppBar(title: Text("الفلترة")),
-      body: Center(child: Text("this is filters page")),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                buildSwitchListTile(
+                  'الرحلات الصيفية فقط',
+                  "إظهار الرحلات في فصل الصيف فقط",
+                  _isInSummer,
+                  (newValue) {
+                    setState(() {
+                      _isInSummer = newValue;
+                    });
+                  },
+                ),
+
+                buildSwitchListTile(
+                  'الرحلات الشتوية فقط',
+                  "إظهار الرحلات في فصل الشتاء فقط",
+                  _isInWinter,
+                  (newValue) {
+                    setState(() {
+                      _isInWinter = newValue;
+                    });
+                  },
+                ),
+
+                buildSwitchListTile(
+                  'الرحلات العائلية ',
+                  "إظهار الرحلات العائلية فقط",
+                  _isForFamily,
+                  (newValue) {
+                    setState(() {
+                      _isForFamily = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
